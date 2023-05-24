ea33f658b1ffe8beca4a24930dc09f8713500312
Alexey Gorbunov <nayfront28@gmail.com>
2023-05-24 15:59:37 +0300

    added SoundManager + SoundExtension

--- AliasApp/AliasApp/AppDelegates/SceneDelegate.swift
+++ AliasApp/AliasApp/AppDelegates/SceneDelegate.swift
@@ -15,6 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        
+        let _ = SoundManager.shared
+        
         coordiantor = AppCoordinator()
         AppCoordinator.window = window
         coordiantor?.start()
---
+++ AliasApp/AliasApp/Services/SoundManager.swift
@@ -0,0 +1,69 @@
+//
+//  SoundManager.swift
+//  AliasApp
+//
+//  Created by NaYfront on 24.05.2023.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundManager {
+    static let shared = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    private init() {
+        prepare()
+    }
+    
+    func playSound(sound: Sound) {
+        if let player = SoundManager.shared.player, player.isPlaying {
+            player.stop()
+        }
+        
+        let soundURL = Bundle.main.path(forResource: sound.rawValue, ofType: "mp3")
+        
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            guard let souldURL = soundURL else {
+                return
+            }
+            
+            player = try AVAudioPlayer(contentsOf: URL(filePath: souldURL))
+            
+            guard let player = player else {
+                return
+            }
+            
+            player.play()
+        } catch {
+            print("something went wrong")
+        }
+    }
+    
+    func prepare() {
+        let soundURL = Bundle.main.path(forResource: "buttonTapping", ofType: "mp3")
+        
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            guard let souldURL = soundURL else {
+                return
+            }
+            
+            player = try AVAudioPlayer(contentsOf: URL(filePath: souldURL))
+            
+            guard let player = player else {
+                return
+            }
+            
+            player.prepareToPlay()
+        } catch {
+            print("something went wrong")
+        }
+    }
+}
